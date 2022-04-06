1ac461cb5b4aee3b77d25ff1a9d07d4506593196
Khai Yi Chin <khaiyichin@gmail.com>
2022-04-06 08:55:10 -0400

    Added parallelization to model evaluation.

--- Project3/analysis/evaluate_models.py
+++ Project3/analysis/evaluate_models.py
@@ -4,26 +4,51 @@ from environment import Environment
 import numpy as np
 import argparse
 from agent_dqn import Agent_DQN
+from joblib import Parallel, delayed
+import timeit
+
+def eval_model(args, model_filename):
+
+    env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
+
+    args.test_model_file_path = model_filename
+    agent = Agent_DQN(env, args)
+
+    # Return rewards for current model name
+    return [model_filename, obtain_rewards(agent, env, total_episodes=100)]
+
 
 def run(args):
     if args.test_dqn: # this is mostly to ensure that the variable exist so that it will be used in AgentDQN
-        env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
 
         pth_files = glob.glob( os.path.join( '*pth') )
         unsorted_pth_datetimes = [os.path.splitext(i)[0][-11:] for i in pth_files]
+        sorted_pth_datetimes_ind = np.argsort(unsorted_pth_datetimes)
+        sorted_pth = [glob.glob( os.path.join( '*pth') )[ind] for ind in sorted_pth_datetimes_ind ]
 
         data_rows = []
 
-        # Iterate through all the models
-        for ind in np.argsort(unsorted_pth_datetimes):
-            model_file_path = glob.glob( os.path.join( '*pth') ) [ind]
-            args.test_model_file_path = model_file_path
-            agent = Agent_DQN(env, args)
-            
-            # Store rewards for current model name
-            reward = obtain_rewards(agent, env, total_episodes=100)
+        if args.parallel:
+            data_rows = Parallel(n_jobs=-1, verbose=100)(delayed(eval_model)(args, model_file_path) for model_file_path in sorted_pth)
 
-            data_rows.append( [model_file_path, reward] )
+        else:
+            start = timeit.default_timer()
+            # Iterate through all the models
+            for ind in np.argsort(unsorted_pth_datetimes):
+                model_file_path = glob.glob( os.path.join( '*pth') ) [ind]
+                args.test_model_file_path = model_file_path
+
+                env = Environment('BreakoutNoFrameskip-v4', args, atari_wrapper=True, test=True)
+                agent = Agent_DQN(env, args)
+
+                # Store rewards for current model name
+                reward = obtain_rewards(agent, env, total_episodes=100)
+
+                data_rows.append( [model_file_path, reward] )
+
+            end = timeit.default_timer()
+
+            print('Elapsed time:', end-start)
 
         output_df = pd.DataFrame(data_rows, columns=['model_filename', 'score'])
         output_df.to_csv('complete_results.csv', index=False)
@@ -49,8 +74,8 @@ def obtain_rewards(agent, env, total_episodes=100):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="DS595/CS525 RL Project 3")
-    parser.add_argument('--test_dqn', action='store_true', help='whether test DQN')
     parser.add_argument('--model_directory', type=str, help='path to model files.')
+    parser.add_argument('--parallel', action='store_true')
 
     args = parser.parse_args()
     
